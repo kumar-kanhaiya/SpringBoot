c803014bc3793d630654e9768752c126bea820f8
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-10 19:24:28 +0530

    code

---
+++ Backend-Learning/src/main/java/SpringBoot/Backend/Learning/Service/UserService.java
@@ -0,0 +1,14 @@
+package SpringBoot.Backend.Learning.Service;
+
+import SpringBoot.Backend.Learning.Bean.UserConfig;
+
+public class UserService {
+
+    UserConfig userConfig;
+
+    public UserService(UserConfig userConfig){
+        this.userConfig = userConfig;
+    }
+
+
+}
