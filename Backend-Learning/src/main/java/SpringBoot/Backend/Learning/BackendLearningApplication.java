66c322d60788ff6ba8fc10e9416695b5935da913
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-10 17:31:24 +0530

    code

---
+++ Backend-Learning/src/main/java/SpringBoot/Backend/Learning/BackendLearningApplication.java
@@ -0,0 +1,15 @@
+package SpringBoot.Backend.Learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLearningApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(BackendLearningApplication.class, args);
+
+	}
+
+}
