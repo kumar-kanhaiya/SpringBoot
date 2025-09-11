7775889e487aaa58981036576222723def0e626d
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-09-11 12:31:19 +0530

    code

--- Backend-Learning/src/main/java/SpringBoot/Backend/Learning/BackendLearningApplication.java
+++ Backend-Learning/src/main/java/SpringBoot/Backend/Learning/BackendLearningApplication.java
@@ -9,21 +9,25 @@ import SpringBoot.Backend.Learning.Classes.PersonInvocationHandler;
 import SpringBoot.Backend.Learning.Service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import java.lang.reflect.Proxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class BackendLearningApplication {
 
 	public static void main(String[] args) {
 
-//        SpringApplication.run(BackendLearningApplication.class, args);
+        SpringApplication.run(BackendLearningApplication.class, args);
         Man mohan = new Man("Mohan",30 , "delhi" , "India");
         ClassLoader mohanClassLoader = mohan.getClass().getClassLoader();
         Class[] interfaces = mohan.getClass().getInterfaces();
         Person proxyMohan = (Person) Proxy.newProxyInstance(mohanClassLoader,interfaces,new PersonInvocationHandler(mohan));
 
         proxyMohan.introduce(mohan.getName());
+
+
 
 	}
 
--- Backend-Learning/src/main/java/SpringBoot/Backend/Learning/Service/UserService.java
+++ Backend-Learning/src/main/java/SpringBoot/Backend/Learning/Service/UserService.java
@@ -1,13 +1,38 @@
 package SpringBoot.Backend.Learning.Service;
 
 import SpringBoot.Backend.Learning.Bean.UserConfig;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 public class UserService {
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public class User{
+        private String name;
+        private int age;
+        private String Address;
+    }
 
+    private User user;
+
+    public void logIn(){
+        System.out.println("Logging user in");
+    }
+
+    public void logOut() throws Exception{
+        System.out.println("Logging user out");
+        throw new Exception("unable to logout the user");
+    }
     UserConfig userConfig;
 
     public UserService(UserConfig userConfig){
         this.userConfig = userConfig;
+    }
+
+    public UserService(){
+        user = new User("Kanhaiya Kumar" , 20 , "Delhi , India");
     }
 
 
