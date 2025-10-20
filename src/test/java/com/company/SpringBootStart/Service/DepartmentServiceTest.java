aeaf185703006a362526cb9d061b433dd9a4f25d
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-20 12:33:21 +0530

    code

---
+++ src/test/java/com/company/SpringBootStart/Service/DepartmentServiceTest.java
@@ -0,0 +1,30 @@
+package com.company.SpringBootStart.Service;
+
+import com.company.SpringBootStart.Entity.Department;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DepartmentServiceTest {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    public void whenValidDepartmentName_thenDepartmentShouldFound(){
+        String departmentName = "IT";
+        Department found = departmentService.fetchDepartmentByName(departmentName);
+
+        assertEquals(departmentName , found.getDepartmentName());
+
+
+    }
+}
