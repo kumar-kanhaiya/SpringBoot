4ccfc45140c6e92c49b49a434c4e57277ed7aa87
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-21 11:52:30 +0530

    code

--- src/test/java/com/company/SpringBootStart/Service/DepartmentServiceTest.java
+++ src/test/java/com/company/SpringBootStart/Service/DepartmentServiceTest.java
@@ -1,10 +1,14 @@
 package com.company.SpringBootStart.Service;
 
 import com.company.SpringBootStart.Entity.Department;
+import com.company.SpringBootStart.repository.DepartmentRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,8 +18,19 @@ class DepartmentServiceTest {
     @Autowired
     private DepartmentService departmentService;
 
+    @MockBean
+    private DepartmentRepository departmentRepository;
+
     @BeforeEach
     void setUp() {
+        Department department = Department.builder()
+                .departmentName("IT")
+                .departmentAdd("Banglore")
+                .departmentCode("IS-35")
+                .departmentId(1L)
+                .build();
+
+        Mockito.when(departmentRepository.findByDepartmentNameIgnoreCase("IT")).thenReturn(department);
     }
 
     @Test
